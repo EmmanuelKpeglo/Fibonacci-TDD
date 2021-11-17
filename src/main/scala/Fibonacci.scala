3d0478f7c7aebf54ad773af7584cd1374f28dc62
EmmanuelKpeglo <emmanuel.kpeglo@turntabl.io>
2021-11-17 13:51:11 +0000

    wrote code to test again

--- src/main/scala/Fibonacci.scala
+++ src/main/scala/Fibonacci.scala
@@ -1,5 +1,26 @@
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
 object Fibonacci {
-  def fibonacci(n : Int) = {
-    20
+
+  def fibonacci(numberOfSequence : Int): mutable.Seq[Int] = {
+    var previousNumber: Int = 0
+    var currentNumber : Int = 1
+    val fibonacciNumbers : ListBuffer[Int] = ListBuffer(previousNumber, currentNumber)
+    var nextNumber : Int = 0
+    var counter = 1
+    while ( counter <= numberOfSequence) {
+      nextNumber = previousNumber + currentNumber
+      fibonacciNumbers.addOne(nextNumber)
+
+      previousNumber = currentNumber
+      currentNumber = nextNumber
+
+      counter += 1
+    }
+    
+    fibonacciNumbers
+
   }
+  
 }
