6feb67a1bf92739a2250eb1de91d9dedcbaf4b9c
EmmanuelKpeglo <emmanuel.kpeglo@turntabl.io>
2021-11-17 14:15:27 +0000

    modified code and tested for only 1 element and it passed

--- src/main/scala/Fibonacci.scala
+++ src/main/scala/Fibonacci.scala
@@ -6,23 +6,31 @@ object Fibonacci extends App {
   def fibonacci(numberOfSequence : Int): mutable.Seq[Int] = {
     var previousNumber: Int = 0
     var currentNumber : Int = 1
-    val fibonacciNumbers : ListBuffer[Int] = ListBuffer(previousNumber, currentNumber)
-    var nextNumber : Int = 0
-    var counter = 1
-    while ( counter < numberOfSequence - 1) {
-      nextNumber = previousNumber + currentNumber
-      fibonacciNumbers.addOne(nextNumber)
 
-      previousNumber = currentNumber
-      currentNumber = nextNumber
-
-      counter += 1
+    if (numberOfSequence == 1) {
+      val fibonacciNumbers : ListBuffer[Int] = ListBuffer(previousNumber)
+      fibonacciNumbers
     }
-    
-    fibonacciNumbers
+    else {
+      val fibonacciNumbers : ListBuffer[Int] = ListBuffer(previousNumber, currentNumber)
+      var nextNumber : Int = 0
+      var counter = 1
+      while ( counter < numberOfSequence - 1) {
+        nextNumber = previousNumber + currentNumber
+        fibonacciNumbers.addOne(nextNumber)
+
+        previousNumber = currentNumber
+        currentNumber = nextNumber
+
+        counter += 1
+      }
+
+      fibonacciNumbers
+
+    }
 
   }
 
-//  println(fibonacci(1))
+  println(fibonacci(8))
   
 }
